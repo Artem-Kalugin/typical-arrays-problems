7fe19af1587c361af7df3647e17fb285b27fc5cd
Artem-Kalugin <Aharlo4@gmail.com>
2020-10-10 20:12:40 +0300

    fix: fix code

--- src/index.js
+++ src/index.js
@@ -1,31 +1,21 @@
-function min(a) {
+exports.min = function min(array) {
     let minimal = a[0];
-    a.foreach((element) => {
+    array.foreach((element) => {
         if (element < minimal) minimal = element;
     });
     return minimal;
-}
-function max(a) {
-    let maximum = a[0];
-    a.foreach((element) => {
-        if (element > maximum) maximum = element;
-    });
-    return maximum;
-}
-function average(a) {
-    let sum = 0;
-    a.foreach((element) => (sum += element));
-    return sum / a.lenght;
-}
-
-exports.min = function min(array) {
-    return 0;
 };
 
 exports.max = function max(array) {
-    return 0;
+    let maximum = a[0];
+    array.foreach((element) => {
+        if (element > maximum) maximum = element;
+    });
+    return maximum;
 };
 
 exports.avg = function avg(array) {
-    return 0;
+    let sum = 0;
+    array.foreach((element) => (sum += element));
+    return sum / a.lenght;
 };
