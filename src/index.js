efe5b1449a5070c6a6469b64a1f7fe3497f6157b
Artem-Kalugin <Aharlo4@gmail.com>
2020-10-10 20:10:34 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-
-exports.min = function min (array) {
-  return 0;
+function min(a) {
+    let minimal = a[0];
+    a.foreach((element) => {
+        if (element < minimal) minimal = element;
+    });
+    return minimal;
+}
+function max(a) {
+    let maximum = a[0];
+    a.foreach((element) => {
+        if (element > maximum) maximum = element;
+    });
+    return maximum;
+}
+function average(a) {
+    let sum = 0;
+    a.foreach((element) => (sum += element));
+    return sum / a.lenght;
 }
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.min = function min(array) {
+    return 0;
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return 0;
+};
+
+exports.avg = function avg(array) {
+    return 0;
+};
